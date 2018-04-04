9231ff5c566874e4bf82e925efab1598c58ca4c8
Anwar Jahan <anwar.jahan19@gmail.com>
2018-04-04 08:18:59 -0400

    add text and button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {door: ''}
+        this.addDoor = this.addDoor.bind(this);
+    }
+
+    componentDidMount() {
+
+    }
+
+    addDoor(e){
+
+        this.setState({door: 'Hello World'});
+    }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +29,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+          <input type="text" id='dname' name='dname' value =''/>
+          <button onclick = {this.addDoor}>Add Door</button>
+          <h3>{this.state.door}</h3>
       </div>
     );
   }
