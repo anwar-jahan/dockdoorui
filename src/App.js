ccc2f32da00f860ea43f2add3565914b695e5e2c
Anwar Jahan <anwar.jahan19@gmail.com>
2018-04-05 11:24:37 -0400

    CORS resolution

--- src/App.js
+++ src/App.js
@@ -6,17 +6,49 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {door: ''}
+        this.state = {
+            doors: [], newdoor: ''
+        };
         this.addDoor = this.addDoor.bind(this);
+        this.formChange = this.formChange.bind(this);
     }
 
     componentDidMount() {
 
+        fetch('http://localhost:8080/api/getAllDoors')
+            .then( results => {
+               return results.json();
+            }).then(data => {
+                let doorKey = data.map((d)=> {
+                     return (
+                         <div key={d.data}>
+                             <p>{d.name}</p>
+                         </div>
+                     )
+                })
+                this.setState({doors: doorKey});
+            })
+
     }
 
-    addDoor(e){
+    addDoor(event){
 
-        this.setState({door: 'Hello World'});
+
+        fetch('http://localhost:8080/api/addDoor', {
+           method: 'POST',
+           headers: {'Accept': 'application/json','Content-Type': 'application/json;charset=UTF-8'},
+            body: JSON.stringify({"name":"Hello World"})//JSON.stringify("Hello")
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+                console.log(error);
+            });
+
+        event.preventDefault();
+    }
+
+    formChange(event) {
+        this.setState({door: event.target.value});
     }
 
   render() {
@@ -30,9 +62,15 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-          <input type="text" id='dname' name='dname' value =''/>
-          <button onclick = {this.addDoor}>Add Door</button>
-          <h3>{this.state.door}</h3>
+          <form>
+              <label>
+                  Door Name:
+            <input ref="doorname" type="text" name='dname' value={this.state.door} onChange={this.formChange}/>
+              </label>
+            <input type="submit" value="Add Door" onClick = {this.addDoor} />
+          </form>
+          <h4>{this.state.doors}</h4>
+          <h1>{this.state.door}</h1>
       </div>
     );
   }
